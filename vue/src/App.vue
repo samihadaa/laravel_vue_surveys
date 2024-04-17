f0e9bf786fceb0f08a18e1071a11fd07c54be3b9
Sami Khiari <samihada.madaraa@gmail.com>
2024-04-17 18:57:57 +0100

    FEAT: Adding vue authentication guards

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-
     <router-view/>
   </header>
 
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
 import Survey from '../views/Survey.vue';
+import store from '../store/index.js';
 
 const routes = [
     {
@@ -12,6 +13,7 @@ const routes = [
         redirect: '/dashboard',
         name:'DefaultLayout',
         component: DefaultLayout,
+        meta: { requiresAuth: true },
         children:[
             { path:'/dashboard',name:'Dashboard',component: Dashboard},
             { path:'/survey',name:'Survey',component: Survey}
@@ -34,4 +36,14 @@ const router = createRouter({
     routes
 });
 
+router.beforeEach((to, from, next) => {
+if(to.meta.requiresAuth && !store.state.user.token){
+    next({name: 'Login'});
+}
+else if(store.state.user.token && (to.name === 'Login' || to.name === 'Register')){
+    next({name: 'Dashboard'});
+}else{
+    next();
+}
+});
 export default router;
